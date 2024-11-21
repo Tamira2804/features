d13d710c00d001d09dd4d4a262328abce9c3fd8d
Tamira2804 <tamira2804@gmail.com>
2024-11-21 09:59:26 +0200

    add Card with inner Glow Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,16 @@
 // import Switcher from './Switcher/Switcher';
 // import BouncyImageRadioGroup from './BouncyImageRadioGroup/BouncyImageRadioGroup';
 // import ModalCallback from './ModalCallBack/ModalCallBack';
-import MenuBurgerRight from './MenuBurgerRight/MenuBurgerRight';
+// import MenuBurgerRight from './MenuBurgerRight/MenuBurgerRight';
 import EngineButton from './EngineButton/EngineButton';
+import CardWithInnerGlow from './CardWithInnerGlow/CardWithInnerGlow';
 
 export const App = () => {
   return (
     <div>
+      <CardWithInnerGlow />
       <EngineButton />
-      <MenuBurgerRight />
+      {/* <MenuBurgerRight /> */}
       {/* <ModalCallback />
       <BouncyImageRadioGroup />
       <Switcher />
---
+++ src/components/CardWithInnerGlow/CardWithInnerGlow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CardWithInnerGlow.css';
+
+const CardWithInnerGlow = () => {
+  return (
+    <div id="body">
+      <div className="card">I'm a card with an inner glow</div>
+    </div>
+  );
+};
+
+export default CardWithInnerGlow;
